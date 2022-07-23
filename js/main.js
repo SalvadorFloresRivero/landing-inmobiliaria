22eac15402e35877f90102133be9ed4b40d4d86c
SalvadorFloresRivero <salvador.flores@insabi.gob.mx>
2022-07-23 18:27:13 -0500

    Implementación de localStorage 2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,91 @@
-//localStorage.setItem('countUsers', '0');
-/*
-localStorage.setItem('mySongs', JSON.stringify(mySongs));
+// Clase Tools
+class Tools {
 
-class MusicPlayer {
-    constructor(querySelector) {
-        this.songs = JSON.parse( localStorage.getItem('mySongs') )
-*/
+    getEntidad(sState){
 
+		switch (sState) {
+			case '1': return "Aguascalientes"
+					break
+			case '2': return "Baja California"
+					break
+			case '3': return "Baja California Sur"
+					break
+			case '4':	return "Campeche"
+					break
+			case '5': return "Coahuila"
+					break
+			case '6': return "Colima"
+					break
+			case '7': return "Chiapas"
+					break
+			case '8': return "Chihuahua"
+					break
+			case '9': return "Ciudad de México"
+					break
+			case '10': return "Durango"
+					break
+			case '11': return "Guanajuato"
+					break
+			case '12': return "Guerrero"
+					break
+			case '13': return "Hidalgo"
+					break
+			case '14': return "Jalisco"
+					break
+			case '15': return "Estado de México"
+					break
+			case '16': return "Michoacán"
+					break
+			case '17': return "Morelos"
+					break
+			case '18': return "Nayarit"
+					break
+			case '19': return "Nuevo León"
+					break
+			case '20': return "Oaxaca"
+					break
+			case '21': return "Puebla"
+					break
+			case '22': return "Querétaro"
+					break
+			case '23': return "Quintana Roo"
+					break
+			case '24': return "San Luis Potosí"
+					break
+			case '25': return "Sinaloa"
+					break
+			case '26': return "Sonora"
+					break
+			case '27': return "Tabasco"
+					break
+			case '28': return "Tamaulipas"
+					break
+			case '29': return "Tlaxcala"
+					break
+			case '30': return "Veracruz"
+					break
+			case '31': return "Yucatán"
+					break
+			case '32': return "Zacatecas"
+					break
+            default: "Ninguna"
+		}
+	}
+    getHouseType(houseType){
+
+		switch (houseType) {
+			case '1': return "Departamento"
+					break
+			case '2': return "Casa"
+					break
+			case '3': return "Terreno"
+					break
+			case '4':	return "Rentar"
+					break
+            default: "Ninguna"
+        }
+    }
+}
 class User {
     constructor(id, name, phon, state, houseType) {
         this.id=id
@@ -22,6 +101,7 @@ const myUsers = []
 class UserCRUD {
 
     constructor() {
+        this.encabezado=resultado.innerHTML
         this.dates = JSON.parse( localStorage.getItem('myUsers') )
         if(this.dates != null){
             let i = 0
@@ -32,77 +112,85 @@ class UserCRUD {
             })
         }
     }
+    loadUser(id,name,phon,state,houseType) {
+
+        document.querySelector('#mod_names').value=name
+        document.querySelector('#mod_phon').value=phon
+        document.querySelector('#mod_state').value=state
+        document.querySelector('#mod_houseType').value=houseType
+
+    }
     showUser(inserUser){
 
-        let contador = parseInt(inserUser.id)
+        let contador = parseInt(inserUser.id)+1
+        let sState = myTools.getEntidad(inserUser.state)
+        let sHouseType = myTools.getHouseType(inserUser.houseType)
 
         resultado.innerHTML += `<tr>
         <td>${contador}</td>
         <td>${inserUser.name}</td>
         <td>${inserUser.phon}</td>
-        <td>${inserUser.state}</td>
-        <td>${inserUser.houseType}</td>
-        <td><img src='img/editar.png' alt='Farheso' width='20' height='20'/></td>
-        <td><img src='img/eliminar.png' alt='Farheso' width='20' height='20'/></td>
+        <td>${sState}</td>
+        <td>${sHouseType}</td>
+        <td><a onclick=\"myUser.loadUser( ${inserUser.id},'${inserUser.name}','${inserUser.phon}',${inserUser.state},${inserUser.houseType});\" style='color:#000000;' data-bs-toggle="modal" data-bs-target="#exampleModal" data-placement='top' title='Editar usuario' style='cursor:pointer'><img src='img/editar.png' alt='Farheso' width='20' height='20'/></a></td>
+        <td><a onclick=\"myUser.deleteUser( ${inserUser.id});\"><img src='img/eliminar.png' alt='Farheso' width='20' height='20'/></a></td>
         </tr>`
 
-    }
+}
     newUser (){
 
-        let lenghtData = 0
-        let id = myUsers.length
+        let id = 0
         let names = document.querySelector('#names').value;
         let phon = document.querySelector('#phon').value;
         let state = document.querySelector('#state').value;
         let houseType = document.querySelector('#houseType').value;
-
-        let inserUser = new User(id,names,phon,state,houseType)
-
-        this.showUser(inserUser)
-        myUsers.push(inserUser)
-
         let dates = JSON.parse( localStorage.getItem('myUsers') )
 
         if(dates == null){
+            let inserUser = new User(id,names,phon,state,houseType)
+            this.showUser(inserUser)
+            myUsers.push(inserUser)
             localStorage.setItem('myUsers', JSON.stringify(myUsers))
         }
         else{
             console.log("Entro")
+
             dates.forEach(elemen=>{
-                    localStorage.removeItem("myUsers")
-                    dates.push(inserUser)
-                    console.log("Entro 2" + dates.name)
-                    localStorage.setItem('myUsers', JSON.stringify(dates))
-
+                id+=1
+                console.log("ids:"+id)
             })
+
+            let inserUser = new User(id,names,phon,state,houseType)
+            localStorage.removeItem("myUsers")
+            dates.push(inserUser)
+            this.showUser(inserUser)
+            localStorage.setItem('myUsers', JSON.stringify(dates))
+
         }
-
-
-        //myUsers.push(inserUser)
-        //https://developer.mozilla.org/es/docs/Learn/JavaScript/Objects/JSON
-
-        //localStorage.setItem('myUsers', JSON.stringify(myUsers))
-
-
-        localStorage.removeItem("MySongs")
-
-        console.log("Nombres: "+ names);
-        console.log("Telefono: "+ phon);
-        console.log("Entidad: "+ state);
-        console.log("Tipo de casa: "+ houseType);
-
-        /* myUsers.forEach(elemen=>{
-            console.log("Arreglo: "+ myUsers[id].id);
-            console.log("Arreglo: "+ myUsers[id].name);
-        }) */
-       // this.showUser(inserUser)
-
     }
-    deleteUser (){
+    deleteUser (idDelete){
+
+        let myUsersDelete = JSON.parse( localStorage.getItem('myUsers') )
+        localStorage.removeItem("myUsers")
+        resultado.innerHTML=this.encabezado
+        const myUsersNuevos = []
+
+        let idcount=0
+        myUsersDelete.forEach(elemen=>{
+            if( myUsersDelete[idcount].id != idDelete ){
+                myUsersNuevos.push(myUsersDelete[idcount])
+                this.showUser(myUsersDelete[idcount])
+                console.log(idcount)
+            }
+            idcount+=1
+        })
+
+        localStorage.setItem('myUsers', JSON.stringify(myUsersNuevos))
 
     }
     updateUser() {
-        let idUpdate = document.querySelector('#id').value;
+
+       /*  let idUpdate = document.querySelector('#id').value;
         let names = document.querySelector('#names').value;
         let phon = document.querySelector('#phon').value;
         let state = document.querySelector('#state').value;
@@ -114,11 +202,14 @@ class UserCRUD {
         myUsersUpdate.forEach(elemen=>{
             if( myUsersUpdate[idcount].id == idUpdate)
             console.log("Este es el objeto a modificar "+ myUsersUpdate[idcount].id);
-        })
+        }) */
+        console.log("Hola desde el update")
 
     }
+
 }
 
 
-//const myMusicPlayer = new MusicPlayer(mySongs,'#titulo')
-const myUser = new UserCRUD( )
+const myTools = new Tools()
+const myUser = new UserCRUD()
+
